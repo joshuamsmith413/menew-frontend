922df80fc93e76e66b0349d4b7262572392af606
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-08-13 23:57:30 -0400

    added restaurant tiles to homepage instead of dropdown on navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,12 +3,26 @@ import React from 'react';
 
 class HomePage extends React.Component {
 
-  render() {
+  renderRestaurantName = () => {
+    return this.props.restaurants.map(rest => {
+      return <div className="restaurantTile">
+        <img src={rest.picture} alt={rest.name} height="300" width="300"/>
+        <div className="restaurantText" onClick={() => this.props.selectRestaurant(rest)}><h2>{rest.name}</h2></div>
+      </div>
+    })
+  }
 
+  //  style ={ { backgroundImage: "url(http://www.eatout.co.za/wp-content/uploads/2015/10/Saxon-fivehundredThePass.jpg)", minHeight: "75vh", backgroundSize: "cover" } }
+
+  render() {
+    console.log(this.props.restaurants)
     return (
-      <div className='HomePage'>
-        <div id="HomePagePic" style ={ { backgroundImage: "url(https://designcontest-com-designcontest.netdna-ssl.com/data/contests/215250/entries/d5fdc19b02f7f67a.jpg)", minHeight: "75vh", backgroundSize: "cover" } } >
+      <div id='HomePage'>
+        <div>
           <h1 id="homeTitle">Welcome to Menew</h1>
+          <div id="restaurantContainer">
+            {this.renderRestaurantName()}
+          </div>
         </div>
 
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,18 +5,19 @@ import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
 
+  // <RestaurantDropdown
+  //   items={this.props.items}
+  //   selectRestaurant={this.props.selectRestaurant}
+  //   restaurant={this.props.restaurant}
+  //   restaurants={this.props.restaurants}/>
+
   render(){
     return(
-        <div className="navbar" style={ {backgroundColor: "#e3f2fd"}}>
+        <div className="navbar">
           <Link to="/"><h1 className="nav-title">Menew</h1></Link>
           <span>
-            <NewRestaurant />
             <Link to="/newitem"> New Item </Link>
-            <RestaurantDropdown
-              items={this.props.items}
-              selectRestaurant={this.props.selectRestaurant}
-              restaurant={this.props.restaurant}
-              restaurants={this.props.restaurants}/>
+            <NewRestaurant />
         </span>
         </div>
     )
